f599bfb7ddf0650d93e5e48323679d746472dbec
mpalmer0028 <mpalmer0028@gmail.com>
2021-05-31 17:51:06 -0400

    Start mesh base beam for powerball

---
+++ Assets/Attacks/MeshBeamSystemScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshBeamSystemScript : MonoBehaviour
+{
+	public Transform Powerball;
+	
+	
+	/// <summary>
+	/// Number of verts for each ring
+	/// </summary>
+	private int RingResolution = 8;
+	private float RingDistance = 1;
+	
+	private Mesh Mesh;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+	    //GetComponent<>
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
